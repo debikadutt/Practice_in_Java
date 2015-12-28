9d3b484e678d4d940e5d7ab7211f1a92dd0c0413
Debika@UB2015 <debikadutt@gmail.com>
2015-12-27 20:19:23 -0500

    updated with comments

--- ReverseArrSpecial.java
+++ ReverseArrSpecial.java
@@ -1,20 +1,24 @@
 package practice.in.java;
 
+//Driver program to reverse a string with special characters
 public class ReverseArrSpecial {
 	public static void main(String[] args) {
 		System.out.println(reverseString("Ab,c,de!$"));
 	}
-
+	
 	public static String reverseString(String input) {
 		char[] inputArr = input.toCharArray();
+		//initialize left and right pointers
 		int l = 0;
 		int r = input.length() - 1;
+		
+		//traverse string from both ends until 'l' and 'r'
 		while (l < r) {
-			if (!Character.isAlphabetic(inputArr[l])) {
+			if (!Character.isAlphabetic(inputArr[l])) {//ignore special characters
 				l++;
 			} else if (!Character.isAlphabetic(inputArr[r])) {
 				r--;
-			} else {
+			} else {//both inputArr[l] and inputArr[r] are not special
 				char tempChar = inputArr[l];
 				inputArr[l] = inputArr[r];
 				inputArr[r] = tempChar;
