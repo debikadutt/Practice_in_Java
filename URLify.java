46dd885ab1228ff0a7b07f584c0111fec85ab6d1
Debika@UB2015 <debikadutt@gmail.com>
2016-01-05 00:12:22 -0500

    Update URLify.java

--- URLify.java
+++ URLify.java
@@ -1,24 +1,23 @@
 package arrays.strings;
 
 public class URLify {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		String str = "Mr John Smith";
-		String result = isReplace(str,13);
+		String result = isReplace(str, 13);
 		System.out.println(result);
 	}
-	
-	public static String isReplace(String s, int a){
+
+	public static String isReplace(String s, int a) {
 		StringBuffer sb = new StringBuffer();
-		for(int i=0; i<s.length(); i++){
-			if(s.charAt(i)== ' '){
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == ' ') {
 				sb.append("%20");
-			}else{
+			} else {
 				sb.append(s.charAt(i));
 			}
 		}
-		
+
 		return sb.toString();
 	}
-	
 
 }
