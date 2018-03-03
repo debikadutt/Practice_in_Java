be6e710ef6b350cb42fd623f0d793a97b9d4f78f
debika dutt <debika dutt>
2018-03-03 14:19:03 -0500

    modified pangrams

--- Pangrams.java
+++ Pangrams.java
@@ -10,6 +10,7 @@ public class Pangrams {
 		isPangram(pangramStr);
 
 	}
+	// main program
 
 	public static void isPangram(String s) {
 		String[] lowerAlpha = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
